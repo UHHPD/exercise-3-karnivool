92eb2296f8111a1bac1b807cd898f5f7318e4e70
xi76suvu <mirijam@ele>
2023-11-15 17:14:31 +0100

    1c Histogramm with Errors

--- poisson.cc
+++ poisson.cc
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 using namespace std;
 
+double poisson(double mu, int k);
 
-double poisson(double mu, int k) {
-    return 0;
-}
 
 int main() {
     ifstream f_daten("datensumme.txt");
@@ -14,19 +13,30 @@ int main() {
     int zaehler[11] = {0};
 
     int n_i;
+    int lengthOfDatensumme = 0;
     while (true) {
         f_daten >> n_i;
         if (f_daten.eof()) { break; }
 
         zaehler[n_i]++;
+        lengthOfDatensumme++;
     }
 
-    ofstream f_hist("hist.txt");
+    ofstream f_hist("histpoi.txt");
+    double mu_ex2 = 3.11538;
     for (int i = 0; i < size(zaehler); i++) {
         cout << i << ":" << zaehler[i] << endl;
-        f_hist << i << " " << zaehler[i] << endl;
+        f_hist << i << " " << zaehler[i] << " " << lengthOfDatensumme * poisson(mu_ex2, i) << endl;
+
     }
+
+    //Aufgabe 1c:
+    
 
     f_daten.close();
     f_hist.close();
+}
+
+double poisson(double mu, int k){
+    return (pow(mu, k) * exp(-mu)) / tgamma(k + 1);
 }
