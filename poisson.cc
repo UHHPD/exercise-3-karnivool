02ed800037e97f9eedadfd750cac9be7ac21965b
TobiFreund <tobias.j.freund@gmail.com>
2023-11-15 16:30:34 +0100

    Aufgabe 1a

--- poisson.cc
+++ poisson.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+using namespace std;
 
 
 double poisson(double mu, int k) {
@@ -8,13 +9,24 @@ double poisson(double mu, int k) {
 }
 
 int main() {
-    using namespace std;
+    ifstream f_daten("datensumme.txt");
 
+    int zaehler[11] = {0};
 
-    ifstream fin("datensumme.txt");
     int n_i;
-    for(int i = 0 ; i < 234 ; ++i) {
-        fin >> n_i;
+    while (true) {
+        f_daten >> n_i;
+        if (f_daten.eof()) { break; }
+
+        zaehler[n_i]++;
     }
-    fin.close();
+
+    ofstream f_hist("hist.txt");
+    for (int i = 0; i < size(zaehler); i++) {
+        cout << i << ":" << zaehler[i] << endl;
+        f_hist << i << " " << zaehler[i] << endl;
+    }
+
+    f_daten.close();
+    f_hist.close();
 }
